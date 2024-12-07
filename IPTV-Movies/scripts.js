ca1ebd48ca3d194164a7f570d643e2923072d668
Giovani-Simple-Dev <167205762+Giovani-Simple-Dev@users.noreply.github.com>
2024-12-07 18:13:07 -0300

    upload

---
+++ IPTV-Movies/scripts.js
@@ -0,0 +1,15 @@
+function redirectLivetv() {
+    window.location.href = '/livetv/index.html';
+}
+
+function redirectMoviestv() {
+    window.location.href = '/movies/index.html';
+}
+
+function redirectSeriestv() {
+    window.location.href = '/series/index.html';
+}
+
+function returnDashboard() {
+    window.location.href = '../index.html';
+}
